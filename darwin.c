91b60d13f6e8ca8768031a13cb6dbc8fa71fd1b7
Darwin Acharya <acharyadarwin5@gmail.com>
2023-03-04 12:22:12 +0545

    initial commit

---
+++ darwin.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+    int a ,b;
+    printf("Enter the two numbers :");
+    scanf("%d%d",&a,&b);
+    int result;
+    char ch,choice;
+
+    do {
+        printf("Enter + to add, - to subtract: ");
+        choice=getch();
+        switch(choice) {
+            case'+':
+                result = a + b;
+                printf("Result: %d\n", result);
+                break;
+            case'-':
+                result = a - b;
+                printf("Result: %d\n", result);
+                break;
+            default:
+                printf("Invalid choice !\n");
+        }
+
+        printf("Do you want to continue? Enter y for yes ( y/n): ");
+        scanf(" %c", &ch);
+    } while(ch == 'y' || ch == 'Y'); 
+    
+    return 0;
+}
